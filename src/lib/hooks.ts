260f6c4770718a7ba9e6861908e5ca29f29f11a9
Walassi Silva <walassi.b@gmail.com>
2024-11-04 18:58:08 -0300

    refactor: implement react-query to `useJobItemDetails` hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,33 @@
 import { useEffect, useState } from "react";
 import { TJobItem, TJobItemDetails } from "./types";
 import { BASE_API_URL } from "./constants";
+import { useQuery } from "@tanstack/react-query";
 
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: TJobItemDetails;
+};
+
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+  const res = await fetch(`${BASE_API_URL}/${id}`);
+  const data = await res.json();
+  return data;
+};
+export function useJobItemDetails(id: number | null) {
+  const { data, isInitialLoading } = useQuery(
+    ["job-item", id],
+    () => (id ? fetchJobItem(id) : null),
+    {
+      enabled: Boolean(id),
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+    }
+  );
+  const jobItemDetails = data?.jobItem;
+  const isLoading = isInitialLoading;
+  return { jobItemDetails, isLoading } as const;
+}
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<TJobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -22,26 +48,6 @@ export function useJobItems(searchText: string) {
     fetchData();
   }, [searchText]);
   return { jobItemsSliced, isLoading, totalCount } as const;
-}
-export function useJobItemDetails(id: number | null) {
-  const [jobItemDetails, setJobItemDetails] = useState<TJobItemDetails | null>(
-    null
-  );
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      const res = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await res.json();
-      setIsLoading(false);
-      setJobItemDetails(data.jobItem);
-    };
-    fetchData();
-  }, [id]);
-
-  return { jobItemDetails, isLoading } as const;
 }
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
